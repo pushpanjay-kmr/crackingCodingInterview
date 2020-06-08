6aacfe902046b51a847b32e61293034fc6529dc4
pushpanjay.kumar <pushpanjay.kumar@yatra.com>
2020-06-08 11:28:59 +0530

    array and string problems

--- java/com/hashinclude/cci/arrayandstring/CheckPermutation.java
+++ java/com/hashinclude/cci/arrayandstring/CheckPermutation.java
@@ -1,7 +1,70 @@
 package com.hashinclude.cci.arrayandstring;
 
+import java.util.Arrays;
+
 /**
  * @author pushpanjay.kumar created on 7/6/20
  */
 public class CheckPermutation {
+
+    /**
+     * Count of each character is compared in string s1 and s2
+     * TC: O(n)
+     * AS: O(1) array of size 128 (ascii length) is used
+     * @param s1 string one
+     * @param s2 string two
+     * @return true if string s1 is permutation of string s2
+     */
+    private static boolean checkPermutation1(String s1, String s2){
+        int n1 = s1.length();
+        int n2 = s2.length();
+        if(n1!=n2){
+            return false;
+        }
+        int []counter = new int[128];
+        for(int i=0;i<n1;i++){
+            int v1 = s1.charAt(i);
+            int v2 = s2.charAt(i);
+            counter[v1]++;
+            counter[v2]--;
+        }
+
+        for(int i=0;i<128;i++){
+            if(counter[i]!=0){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private static String sortString(String s){
+        char[] charString = s.toCharArray();
+        Arrays.sort(charString);
+        return new String(charString);
+    }
+
+    /**
+     * Both strings are sorted and then compared
+     * TC: O(nlogn)
+     * AS: O(1)
+     * @param s1 string one
+     * @param s2 string two
+     * @return true if string s1 is permutation of string s2
+     */
+    private static boolean checkPermutation2(String s1, String s2){
+        if(s1.length()!=s2.length()){
+            return false;
+        }
+        return sortString(s1).equals(sortString(s2));
+    }
+
+    public static void main(String[] args) {
+        System.out.println(checkPermutation1("abcde", "abc"));
+        System.out.println(checkPermutation1("abcde", "eabdc"));
+        System.out.println(checkPermutation1("", ""));
+
+        System.out.println(checkPermutation2("abcde", "abc"));
+        System.out.println(checkPermutation2("abcde", "eabdc"));
+        System.out.println(checkPermutation2("", ""));
+    }
 }
--- java/com/hashinclude/cci/arrayandstring/IsUnique.java
+++ java/com/hashinclude/cci/arrayandstring/IsUnique.java
@@ -4,4 +4,55 @@ package com.hashinclude.cci.arrayandstring;
  * @author pushpanjay.kumar created on 7/6/20
  */
 public class IsUnique {
+
+    /**
+     * Using array of size 128 to keep track of visited character
+     * TC; O(n)
+     * AS: O(1) => array of size 128
+     * @param s input string
+     * @return true if input string has all unique character
+     */
+    private static boolean isUnique1(String s){
+        if(s.length()>128){
+            return false;
+        }
+        boolean[] visited = new boolean[128];
+        for(int i=0;i<s.length();i++){
+            int v = s.charAt(i);
+            if(visited[v]){
+                return false;
+            }
+            visited[v] = true;
+        }
+        return true;
+    }
+
+    /**
+     * Assumption : all the character of string s is present in small font(i.e. a-z)
+     * Better than approach1, further space complexity is reduced to a size of single int variable
+     * Using bit vector checker to keep track of visited character
+     * TC: O(n)
+     * AS:: O(1)
+     * @param s input string
+     * @return true if input string has all unique character
+     */
+    private static boolean isUnique2(String s){
+        // using each bit of variable checker as a flag for character a(0th bit) to z
+        int checker = 0;
+        for(int i=0;i<s.length();i++){
+            int v = s.charAt(i) - 'a';
+            if((checker & (1 << v)) > 0){
+                return false;
+            }
+            checker |= (1 << v);
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isUnique1("abcde"));
+        System.out.println(isUnique1("aba"));
+        System.out.println(isUnique2("a"));
+        System.out.println(isUnique2("aba"));
+    }
 }
